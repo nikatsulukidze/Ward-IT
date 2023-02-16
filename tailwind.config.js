01a37b2e5a85e9d2162d69a247d122355fe5c79b
nikatsulukidze <81852435+nikatsulukidze@users.noreply.github.com>
2023-02-16 21:23:58 +0400

    Add files via upload

---
+++ tailwind.config.js
@@ -0,0 +1,28 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./*.{html,js}"],
+  theme: {
+    extend: {},
+    
+
+  },
+  plugins: [],
+  
+  theme: {
+    colors: {
+      transparent: 'transparent',
+      current: 'currentColor',
+      'wardSilver': '#DFDFDF',
+      'wardGreen': '#5EBBA8',
+      'midnight': '#121063',
+      'metal': '#565584',
+      'tahiti': '#3ab7bf',
+      'silver': '#ecebff',
+      'bubble-gum': '#ff77e9',
+      'bermuda': '#78dcca',
+    },
+  },
+}
+
+
+
